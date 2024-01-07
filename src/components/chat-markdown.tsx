cf0043a4ed9f813a4e330878d6289057570b21d9
madhavsikka <madhavsikka99@gmail.com>
2024-01-07 21:35:29 +0530

    add syntax highlight

--- src/components/chat-markdown.tsx
+++ src/components/chat-markdown.tsx
@@ -1,36 +1,47 @@
+import { useMemo, type ReactNode } from "react"
+import reactNodeToString from "react-node-to-string"
 import { Prism as SyntaxHighlighter } from "react-syntax-highlighter"
 import { coldarkDark } from "react-syntax-highlighter/dist/esm/styles/prism"
 
 import ReactMarkdown from "~libs/react-markdown.min.js"
+
+function CustomCode(props: { children: ReactNode; className?: string }) {
+  const code = useMemo(
+    () => reactNodeToString(props.children),
+    [props.children]
+  )
+
+  const match = /language-(\w+)/.exec(props.className || "")
+
+  return (
+    <div className="flex flex-col">
+      <SyntaxHighlighter
+        PreTag="div"
+        children={String(code).replace(/\n$/, "")}
+        language={match?.[1] ?? ""}
+        style={coldarkDark}
+        customStyle={{
+          borderRadius: "0.5rem"
+        }}
+      />
+    </div>
+  )
+}
 
 export const ChatMarkdown = ({ children }) => {
   return (
     <ReactMarkdown
       className={`markdown-custom-styles`}
       components={{
-        code(props) {
-          const { children, className, node, ...rest } = props
-          const match = /language-(\w+)/.exec(className || "")
-          return match ? (
-            <SyntaxHighlighter
-              {...rest}
-              PreTag="div"
-              children={String(children).replace(/\n$/, "")}
-              language={match[1]}
-              style={coldarkDark}
-              customStyle={{
-                padding: "0.5rem 1rem",
-                borderRadius: "0.5rem",
-                fontSize: "0.875rem",
-                lineHeight: "1.25rem",
-                overflow: "auto"
-              }}
-            />
-          ) : (
-            <code {...rest} className={className}>
-              {children}
-            </code>
-          )
+        code: ({ node, inline, className, children, ...props }) => {
+          if (inline) {
+            return (
+              <code className={className} {...props}>
+                {children}
+              </code>
+            )
+          }
+          return <CustomCode className={className}>{children}</CustomCode>
         }
       }}>
       {children}
